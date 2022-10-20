7b2b2cbe89bbcf15c90c21da4ee0c8716a5b8e8e
Bryan <37481349+bryare@users.noreply.github.com>
2022-10-20 12:35:04 -0500

    add image preview

--- medusa-store/src/pages/portfolioinfo.js
+++ medusa-store/src/pages/portfolioinfo.js
@@ -5,11 +5,4 @@ export const portfolio = [
     image: "/1.png",
     href: "https://bryare.github.io/",
   },
-  {
-    name: "piece № Ø2: 'Fuchsia Dreams'",
-    content:
-      "Piece No 02 is the latest iteration of my website, it comes in a black and white color scheme with hints of fuchsia",
-    image: "/2.png",
-    href: "https://bryare.com/",
-  },
 ]
